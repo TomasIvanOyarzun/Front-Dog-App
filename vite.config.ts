484c625ceb1b43ffa10f8658c589878c1f430c70
Tomas Ivan Oyarzun <65320107+TomasIvanOyarzun@users.noreply.github.com>
2023-01-17 17:46:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ["styled-components", { "ssr": true, "displayName": false, "minify": true }]],
+  plugins: [react()],
 })
 
