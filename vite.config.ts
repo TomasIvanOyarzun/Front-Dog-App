57430f5ce0173c0de55c2e068a73e2e2b2ce10b1
tomas oyarzun <xilent312@gmail.com>
2023-01-17 19:02:03 -0300

    npm i vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [ react({
     babel: {
-      presets: ['@babel/preset-typescript'],
       plugins: [
-        '@babel/plugin-transform-typescript',
-        [
-          'babel-plugin-styled-components',
-          {
-            ssr: false,
-            pure: true,
-            displayName: true,
-            fileName: false,
-          },
-        ],
-      ],
-    },
-  }),],
+        ['babel-plugin-styled-components', { ssr: false, pure: true, displayName: true, fileName: true }]
+  ]}})],
 })
 
