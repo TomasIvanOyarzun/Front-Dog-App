84f21dda5ed1e52c30fd198fe0aa4dddd64a702f
Tomas Ivan Oyarzun <65320107+TomasIvanOyarzun@users.noreply.github.com>
2023-01-17 17:23:14 -0300

    Update MenuComment.tsx

--- src/Pages/DogDetail/Comment/MenuComment.tsx
+++ src/Pages/DogDetail/Comment/MenuComment.tsx
@@ -8,7 +8,6 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import { Typography } from '@mui/material';
 import DialogComment from './DialogComment';
-import { commentType } from './Comment';
 import { CommentResponse, userForCommentMath } from '../../../feactures/user/UserSlice';
 
 interface Props {
