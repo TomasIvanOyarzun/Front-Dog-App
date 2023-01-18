e14f35311c1fc9d1d5a029e7eed2a96287df3ef8
tomas oyarzun <xilent312@gmail.com>
2023-01-18 19:02:41 -0300

    update

--- src/Pages/Inicio/Inicio.tsx
+++ src/Pages/Inicio/Inicio.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import CarruselN from '../../components/Carrusel/CarruselN'
 import { Box } from '@mui/system'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button';
-import CardSlice from './CardInicio/CardSlice'
-
 import Welcome from './Welcome/Welcome'
-import dog_black from '../../images/dog-plus-dog-black.png'
 import ImageList from './ImageList/ImageList';
-import { useWidthScreen } from '../../hooks/customHooks';
-
-import black_dog from '../../images/black-dog.webp'
 import Developer from './Developer/Developer';
 import SearchDogInfo from './SearchDog/SearchDogInfo';
-const Inicio = () => {
+import { useFetchConfirmAccountMutation } from '../../feactures/user/UserSlice';
+import { useParams } from 'react-router-dom';
 
-  const {width } = useWidthScreen()
+
+const Inicio = () => {
+ 
+  const [fetchConfirm] = useFetchConfirmAccountMutation()
+  const [responseBack , setResponseBack] = React.useState<{error : boolean, msg: string}>()
+   const {token} = useParams()
+
+
+  React.useEffect(() => {
+      if(token !== undefined) {
+        fetchConfirm(token).unwrap().then(response => setResponseBack(response) )
+        .catch(error => setResponseBack(error  ))
+      }
+        
+  }, [])
+
  
   return (
     <>
--- src/Pages/Inicio/MsgDialogConfirmEmail/DiagloMsgEmailConfirm.tsx
+++ src/Pages/Inicio/MsgDialogConfirmEmail/DiagloMsgEmailConfirm.tsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Alert from '@mui/material/Alert';
 
-const DiagloMsgEmailConfirm = () => {
+interface Props {
+    error : boolean 
+   
+}
+const DiagloMsgEmailConfirm = ({error} : Props) => {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+        if (error === false) {}
+      setOpen(false);
+    };
+
   return (
-    <div>DiagloMsgEmailConfirm</div>
+    <div>
+  
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {"Account confirmation"}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {error ?  <Alert variant="filled" severity="error">
+        
+          We couldn't validate this account because the confirmation token was already verified or doesn't exist
+      </Alert> :  <Alert variant="filled" severity="success">
+      Thank you for creating an account in DOG APP, we tell you that it is already activated to be able to log in
+      </Alert>}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+        <Button onClick={handleClose} autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </div>
   )
 }
 
