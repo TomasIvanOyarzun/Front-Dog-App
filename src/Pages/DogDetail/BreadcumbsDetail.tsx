e5ce540411c01e41a10070eb2c71cd975de11177
tomas oyarzun <xilent312@gmail.com>
2023-01-18 01:03:08 -0300

    icon

--- src/Pages/DogDetail/BreadcumbsDetail.tsx
+++ src/Pages/DogDetail/BreadcumbsDetail.tsx
@@ -4,6 +4,8 @@ import styled from '@mui/material/styles/styled';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Chip from '@mui/material/Chip';
 import HomeIcon from '@mui/icons-material/Home';
+
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import PetsIcon from '@mui/icons-material/Pets';
 import { Link } from 'react-router-dom';
 
